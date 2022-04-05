6bffac4ba07e37d5eb1ca36d90414db9b375f3df
scmcca <scott@mobilitydata.org>
2022-04-05 17:41:42 -0400

    code

--- scripts/fetchdata.sh
+++ scripts/fetchdata.sh
@@ -83,3 +83,181 @@ sed -i.bak "s,includes https://,includes http://,g" docs/realtime/proto.md
 
 # remove all temporary .bak files
 find . -name "*.bak" -type f -delete
+
+##PASTE OF ORIGINAL
+
+# GTFS Schedule
+
+## GTFS Schedule changes
+rm docs/schedule/changes/*
+
+curl https://raw.githubusercontent.com/google/transit/master/gtfs/CHANGES.md -o docs/schedule/changes/changes.md
+
+### split changes file by heading level 3 (requires installation of gcsplit: https://christiantietze.de/posts/2019/12/markdown-split-by-chapter/)
+
+new_header='# Changes'
+sed -i.bak "1s/^/$new_header\n\n/" docs/schedule/changes/changes.md
+
+### bump up headers
+sed -i.bak 's/###/##/g' docs/schedule/changes/changes.md
+
+### convert heading level 3 to bold
+sed -i.bak '/^### */ s/$/&**<br>/' docs/schedule/changes/changes.md
+sed -i.bak 's/### /**/g' docs/schedule/changes/changes.md
+sed -i.bak 's/##/#/g' docs/schedule/changes/changes.md
+find . -name "*.bak" -type f -delete
+
+gcsplit --prefix='changes' --suffix-format='%02d.md' docs/schedule/changes/changes.md /^'# '/ "{*}"
+rm -r docs/schedule/changes/changes.md
+rm -r changes00.md
+
+mv changes* docs/schedule/changes
+
+### rename split changes files
+strings=(
+    index
+    process
+    guiding-principles
+    revision-history
+)
+files=(docs/schedule/changes/*)
+let "count=1"
+for newname in "${strings[@]}"; do
+    mv "${files[$count]}" "docs/schedule/changes/$newname.md"
+    let "count++" 
+done
+
+mv docs/schedule/changes/process.md docs/schedule/changes/guiding-principles.md docs/schedule/changes/revision-history.md docs/schedule
+
+echo "<br><div class="landing-page">
+    <a class="button" href="../process">Specification Amendment Process</a><a class="button" href="../guiding-principles">Guiding Principles</a><a class="button" href="../revision-history">Revision History</a>
+</div>" >> docs/schedule/changes/index.md
+
+
+# GTFS Realtime
+
+## GTFS Realtime feed entities
+
+### Feed Entities
+curl https://raw.githubusercontent.com/google/transit/master/gtfs-realtime/spec/en/feed-entities.md -o docs/realtime/feed-entities/index.md
+new_header='# Feed Entities'
+sed -i.bak "1s/^/$new_header\n\n/" docs/realtime/feed-entities/index.md
+
+### Trip Updates
+curl https://raw.githubusercontent.com/google/transit/master/gtfs-realtime/spec/en/trip-updates.md -o docs/realtime/trip-updates.md
+new_header='# Trip Updates'
+sed -i.bak "1s/^/$new_header\n\n/" docs/realtime/trip-updates.md
+
+### Trip Updates
+curl https://raw.githubusercontent.com/google/transit/master/gtfs-realtime/spec/en/service-alerts.md -o docs/realtime/service-alerts.md
+new_header='# Service Alerts'
+sed -i.bak "1s/^/$new_header\n\n/" docs/realtime/service-alerts.md
+
+### Vehicle Positions
+curl https://raw.githubusercontent.com/google/transit/master/gtfs-realtime/spec/en/vehicle-positions.md -o docs/realtime/vehicle-positions.md
+new_header='# Vehicle Positions'
+sed -i.bak "1s/^/$new_header\n\n/" docs/realtime/vehicle-positions.md
+
+## GTFS Realtime Feed Examples
+
+curl https://raw.githubusercontent.com/google/transit/master/gtfs-realtime/spec/en/examples/alerts.asciipb -o docs/realtime/temp-alerts.md
+curl https://raw.githubusercontent.com/google/transit/master/gtfs-realtime/spec/en/examples/trip-updates-full.asciipb -o docs/realtime/temp-updates.md
+
+ALERTS=docs/realtime/temp-alerts.md
+UPDATES=docs/realtime/temp-updates.md
+
+echo "# Feed Examples
+
+The following examples show a textual representation of feeds. During development it is more convenient to produce ASCII protocol buffer output for easier debugging. You can compare your text output with these examples to check for the validity of data.
+
+## Service alert
+The following example is an ASCII representation of an Alert feed.
+
+\`\`\`python
+`cat $ALERTS`
+\`\`\`
+
+## Trip Update
+The following example is an ASCII representation of an full-dataset Trip Update feed.
+
+\`\`\`python
+`cat $ALERTS`
+\`\`\`
+
+" > docs/realtime/feed-examples.md
+
+rm docs/realtime/temp*
+
+## GTFS Realtime changes
+
+# curl https://raw.githubusercontent.com/google/transit/master/gtfs-realtime/CHANGES.md 
+
+# awesome-transit resource list
+rm docs/resources/*
+curl https://raw.githubusercontent.com/CUTR-at-USF/awesome-transit/master/README.md -o docs/resources/awesome.md
+
+## remove toc
+# sed -i.bak -e '9,39d' docs/resources/awesome.md
+
+## split awesome by heading level 2 (requires installation of gcsplit: https://christiantietze.de/posts/2019/12/markdown-split-by-chapter/)
+gcsplit --prefix='awesome' --suffix-format='%02d.md' docs/resources/awesome.md /^'## '/ "{*}"
+mv awesome* docs/resources
+rm -r docs/resources/awesome.md
+
+## split resources by heading level 3 (requires installtion of gcsplit: https://christiantietze.de/posts/2019/12/markdown-split-by-chapter/)
+gcsplit --prefix='resources' --suffix-format='%02d.md' docs/resources/awesome00.md /^'### '/ "{*}"
+mv resources* docs/resources
+rm -r docs/resources/awesome00.md
+
+## bump up headers
+sed -i.bak 's/##/#/g' docs/resources/awesome*
+sed -i.bak 's/###/#/g' docs/resources/resources*
+
+## remove all temporary .bak files
+find . -name "*.bak" -type f -delete
+
+## rename split resources files
+strings=(
+    license
+    about
+    temp
+    toc
+    getting-started
+    community
+    data
+    software-for-creating-apis
+    agency-tools
+    hardware
+    apps
+    sdk
+    visualizations
+    gtfs
+    gtfs-realtime
+    siri
+    multimodal
+    other
+)
+files=(docs/resources/*)
+let "count=1"
+for newname in "${strings[@]}"; do
+    mv "${files[$count]}" "docs/resources/$newname.md"
+    let "count++" 
+done
+
+## replace headers
+new_header='# Resources'
+sed -i.bak "1 s/.*/$new_header/" docs/resources/temp.md
+new_header='# Other Resources'
+sed -i.bak "1 s/.*/$new_header/" docs/resources/other.md
+
+sed -i.bak 's/# Table of Contents/## Table of Contents/g' docs/resources/toc.md
+sed -i.bak 's/# About/## About/g' docs/resources/about.md
+sed -i.bak 's/# License/## License/g' docs/resources/license.md
+
+cat docs/resources/temp.md docs/resources/toc.md docs/resources/about.md docs/resources/license.md > docs/resources/index.md
+
+rm -r docs/resources/temp.md docs/resources/toc.md docs/resources/license.md docs/resources/about.md
+
+
+## remove all temporary .bak files
+find . -name "*.bak" -type f -delete
