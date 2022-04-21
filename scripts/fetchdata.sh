aef58d34ab36887ee22cb44b0948ce5ac750934b
scmcca <62957380+scmcca@users.noreply.github.com>
2022-04-21 12:06:11 -0400

    configure simpleen example in workflow

--- scripts/fetchdata.sh
+++ scripts/fetchdata.sh
@@ -264,3 +264,18 @@ find . -name "*.bak" -type f -delete
 # ## Resources TOC
 
 # # curl https://raw.githubusercontent.com/google/transit/master/gtfs-realtime/CHANGES.md 
+
+# TRANSLATIONS
+
+find . -name "*.id.md" -type f -delete
+find . -name "*.de.md" -type f -delete
+find . -name "*.es.md" -type f -delete
+find . -name "*.fr.md" -type f -delete
+find . -name "*.pr_BR.md" -type f -delete
+find . -name "*.ru.md" -type f -delete
+find . -name "*.zh.md" -type f -delete
+find . -name "*.zh_TW.md" -type f -delete
+find . -name "*.ja.md" -type f -delete
+find . -name "*.ko.md" -type f -delete
+
+yarn simpleen translate
