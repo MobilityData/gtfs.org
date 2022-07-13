18fa8dad7ebb1b29e26f6ab309da91afa732d3d0
scmcca <scott@mobilitydata.org>
2022-07-13 11:56:48 -0400

    not translated base

---
+++ scripts/nottranslated.sh
@@ -0,0 +1,15 @@
+
+
+id "Terjemahan untuk halaman ini ke dalam bahasa Indonesia belum tersedia."
+de "Für diese Seite sind noch keine Übersetzungen ins Deutsche verfügbar."
+es "La traducción de esta página al español aún no está disponible."
+fr "Les traductions de cette page en français ne sont pas encore disponibles."
+pr_BR "As traduções para esta página em português ainda não estão disponíveis."
+ru "Переводы этой страницы на русский язык пока недоступны."
+zh "本页尚未有中文翻译。"
+ja "このページの日本語訳はまだありません。"
+ko "이 페이지에 대한 한국어 번역은 아직 제공되지 않습니다."
+
+!!! Warning ""
+
+    [string]
