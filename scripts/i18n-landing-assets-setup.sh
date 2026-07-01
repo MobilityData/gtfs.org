4c63b2accff17a6e886c3ca55e355dfe6aee848b
Frédéric Simard <frederic@mobilitydata.org>
2026-07-01 09:25:09 -0400

    Remove Fares V2 directory copying from script (#681)

--- scripts/i18n-landing-assets-setup.sh
+++ scripts/i18n-landing-assets-setup.sh
@@ -6,7 +6,6 @@ ALL_LANGUAGES=("fr" "es" "ja" "de" "id" "ko" "pt" "ru" "zh" "zh-TW")
 LANGUAGES_TO_COPY_TO=("fr" "es" "ja")
 
 RESOURCES_DIR="docs/en/resources"
-FARES_V2_DIR="docs/en/documentation/schedule/examples/fares/"
 GOVERNANCE_DIR="docs/en/community/governance/gtfs-schedule-governance"
 
 echo "" && echo ""
@@ -28,18 +27,16 @@ done
 # Once translated this step needs to be removed.
 
 echo "" && echo ""
-echo "Copying the Resources, Fares v2 examples and Governance sections..."
+echo "Copying the Resources and Governance sections..."
 # Loop through the languages that are translated and copy the english files.
 for lang in "${LANGUAGES_TO_COPY_TO[@]}"; do
   DEST_DIR_RESOURCES="docs/$lang"
-  DEST_DIR_FARES_V2="docs/$lang/documentation/schedule/examples/fares/"
   DEST_DIR_GOVERNANCE="docs/$lang/community/governance/"
   
   # Copy the source directory to the destination directory
   cp -r "$RESOURCES_DIR" "$DEST_DIR_RESOURCES"
-  cp -r "$FARES_V2_DIR" "$DEST_DIR_FARES_V2"
   cp -r "$GOVERNANCE_DIR" "$DEST_DIR_GOVERNANCE"
   
-  echo "Folders copied successfully to $DEST_DIR_RESOURCES & $DEST_DIR_FARES_V2 & $DEST_DIR_GOVERNANCE"
+  echo "Folders copied successfully to $DEST_DIR_RESOURCES & $DEST_DIR_GOVERNANCE"
 done
 echo "" && echo ""
